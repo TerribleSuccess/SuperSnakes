b4be4860a77b78283cd06cf6dd8b23b19149747c
TerribleSuccess <146784390+TerribleSuccess@users.noreply.github.com>
2025-11-17 17:04:51 +0000

    add and removed some function declaratios

--- main.h
+++ main.h
@@ -1,6 +1,12 @@
 #ifndef MAIN_H_
 #define MAIN_H_
 
+void initializeColors(void);
+
 void printMarioSide(int, int);
-void printTitle(int,int);
+void printTitle(void);
+void printStart(int,int);
+
+void jump(int, int);
+
 #endif
