7e58e8a57b2b58992638d5bbd03d89ed2534bd3c
TerribleSuccess <146784390+TerribleSuccess@users.noreply.github.com>
2025-11-17 17:04:31 +0000

    displays title screen now

--- main.c
+++ main.c
@@ -1,36 +1,46 @@
 #include <curses.h>
 #include <sys/ioctl.h>
-
+#include <unistd.h>
 #include "main.h"
 
-double marioY;
-double marioX;
-
 int main(){
+    int width;
+    int height;
     struct winsize wbuf;
-
     if(ioctl(0, TIOCGWINSZ, &wbuf) != - 1){
-        marioX = wbuf.ws_col/2;
-        marioY = wbuf.ws_row/2;
+        width = wbuf.ws_col;
+        height = wbuf.ws_row;
     }
 
-    initscr(); //Initialize screen
+    // 1 time calls
+    initscr();
     cbreak();
     noecho();
     keypad(stdscr, TRUE);
+    initializeColors();
+    usleep(1000);
 
-    int ch = getch();
-    int on = 1;
-    while (on){
-        char c = getchar();
-        // switch(c){
-        //     case 'w': jump(); break;
-        //     case 'a': jump(); break;
-        //     case 's': jump(); break;
-        //     case 'd': jump(); break;
-        //     case 'q': on = 0; break;
-        // }
+    clear();
+    printMarioSide(width/8, (height*3)/4);
+    refresh();
+    printTitle();
+    printMarioSide(width/8, (height*3)/4);
+
+    int startMenuToggle = 1;
+    while(startMenuToggle){
+        int ch = getch();
+        switch(ch){
+            case 'w':jump(width/8, (height*3)/4); break;
+            case 'a':jump(width/8, (height*3)/4); break;
+            case 's':jump(width/8, (height*3)/4); break;
+            case 'd':jump(width/8, (height*3)/4); break;
+            case 'q':startMenuToggle = 0; break;
+        }
     }
+
+    //Game Code!
+
+    //End Of Program
     endwin();
     return 1;
 }
