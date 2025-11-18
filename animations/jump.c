38e49ae0ff83a11836fec6e7244938a2046f0de1
TerribleSuccess <146784390+TerribleSuccess@users.noreply.github.com>
2025-11-18 14:21:43 +0000

    buggy but now interacts with objects

--- animations/jump.c
+++ animations/jump.c
@@ -6,15 +6,23 @@
 void jump(int xIn, int yIn){
     int x = xIn;
     int y = yIn;
-    for(int i = 0; i < 24; i++){
-        printMarioSide(x, y-i);
-        marioBottomMask(x, y-i);
+
+    int iTwo = 0;
+    for(int i = 0; i < 30; i++){
+        if(PAIR_NUMBER(mvinch(y-i, x+16)) == COLOR_PAIR_BABYBLUE){
+            printMarioSide(x, y-iTwo);
+            marioBottomMask(x, y-iTwo);
+            iTwo++;
+        }
         refresh();
-        usleep(4000+(i*250));
+        usleep(1+(i*i*50));
     }
-    for(int i = 25; i > 0; i--){
-        printMarioSide(x, y-i);
-        marioTopMask(x, y-i);
+    for(int i = 0; i < 29; i++){
+        if(PAIR_NUMBER(mvinch(y+17, x+16)) == COLOR_PAIR_BABYBLUE){
+            printMarioSide(x, y-iTwo);
+            marioTopMask(x, y-iTwo);
+            iTwo--;
+        }
         refresh();
         usleep(12500-(i*250));
     }
