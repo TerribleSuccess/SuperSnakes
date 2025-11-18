26b304ab0ec2a26d7de15de4e43f83ede460bf52
TerribleSuccess <146784390+TerribleSuccess@users.noreply.github.com>
2025-11-18 13:43:46 +0000

    added definitions for masks and more colors

--- main.h
+++ main.h
@@ -3,19 +3,24 @@
 
 #define COLOR_PAIR_WHITE 1
 #define COLOR_PAIR_RED 2
-#define COLOR_PAIR_ORANGE 3
+#define COLOR_PAIR_SKIN 3
 #define COLOR_PAIR_BLUE 4
 #define COLOR_PAIR_BROWN 5
 #define COLOR_PAIR_BLACK 6
 #define COLOR_PAIR_YELLOW 7
+#define COLOR_PAIR_BABYBLUE 8
 
 #define COLOR_BROWN 8
-#define COLOR_ORANGE 9
+#define COLOR_SKIN 9
+#define COLOR_BABYBLUE 10
 
 void initializeColors(void);
-void printMarioSide(int, int);
 void printTitle(void);
 void printStart(int,int);
+
+void printMarioSide(int, int);
+void marioTopMask(int, int);
+void marioBottomMask(int, int);
 
 void jump(int, int);
 
