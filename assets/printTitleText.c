2ca1f63a1ea4c3b16ac16bf3b45b6d6df52f920f
TerribleSuccess <146784390+TerribleSuccess@users.noreply.github.com>
2025-11-22 02:54:13 +0000

    added instructions text to title screen

---
+++ assets/printTitleText.c
@@ -0,0 +1,27 @@
+#include <curses.h>
+#include "../main.h"
+
+
+void printTitleText(int xIn, int yIn){
+    int x = xIn;
+    int y = yIn;
+
+    attron(COLOR_PAIR(COLOR_PAIR_RED));
+    for (int j = 0; j < 6; j++){
+        for (int i = 0; i < 60; i++){
+            mvprintw(y+j, x+i, " ");
+        }
+    }
+    
+    attron(COLOR_PAIR(COLOR_PAIR_TITLETEXT));
+    for (int j = 0; j < 4; j++){
+        for (int i = 0; i < 56; i++){
+            mvprintw(y+j+1, x+i+2, " ");
+        }
+    }
+    
+    mvprintw(y+1, x+14,"Created By: Ryan C and Michael C");
+    mvprintw(y+2, x+6,"To control mario utilize WASD or the arrow keys");
+    mvprintw(y+3, x+13,"To exit the game press the esc key");
+    mvprintw(y+4, x+18,"Press any key to start!");
+}
