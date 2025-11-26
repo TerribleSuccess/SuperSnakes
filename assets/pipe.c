f5eaff3d2552accf2962c1c160b3050482eeb1cd
TerribleSuccess <146784390+TerribleSuccess@users.noreply.github.com>
2025-11-26 04:57:11 +0000

    enabled adding objects for mario to move arround and on

---
+++ assets/pipe.c
@@ -0,0 +1,68 @@
+#include <curses.h>
+#include "../main.h"
+
+void deletePipe(int xIn, int yIn, int height){
+    int x = xIn;
+    int y = yIn;
+
+    for (int j = 0; j < 7; j++){
+        for (int i = 0; i < 40; i++){
+        safePrint(y+j, x+i, "  ", COLOR_PAIR_BABYBLUE);
+        }
+    }
+    for (int j = 0; j < height-4; j++){
+        for (int i = 0; i <36; i++){
+            safePrint(y+j, x+i+2, "  ", COLOR_PAIR_BABYBLUE);
+        }
+    }
+}
+
+int printPipe(int xIn, int yIn, int height, int sequence){
+    int x = xIn;
+    int y = yIn;
+
+    switch (sequence){
+    case 0:
+        for (int i = 0; i < 7; i++){
+           safePrint(y+i, x, " ", COLOR_PAIR_BLACK);
+        }
+        return 1;
+        break;
+
+    case 1:
+        safePrint(y, x, " ", COLOR_PAIR_BLACK);
+        for (int i = 1; i < 6; i++){
+           safePrint(y+i, x, " ", COLOR_PAIR_GREEN);
+        }
+        for (int i = 6; i < height; i++){
+           safePrint(y+i, x, " ", COLOR_PAIR_BLACK);
+        }
+        break;
+
+    case 2:
+        safePrint(y, x, " ", COLOR_PAIR_BLACK);
+        for (int i = 1; i < height; i++){
+           safePrint(y+i, x, " ", COLOR_PAIR_GREEN);
+        }
+        return 1;
+        break;
+
+    case 3:
+        safePrint(y, x, " ", COLOR_PAIR_BLACK);
+        for (int i = 1; i < 6; i++){
+           safePrint(y+i, x, " ", COLOR_PAIR_GREEN);
+        }
+        for (int i = 6; i < height; i++){
+           safePrint(y+i, x, " ", COLOR_PAIR_BLACK);
+        }
+        break;
+
+
+    case 4:
+        for (int i = 0; i < 7; i++){
+           safePrint(y+i, x, " ", COLOR_PAIR_BLACK);
+        }
+        return 1;
+        break;
+    }
+}
---
+++ assets/safePrint.c
@@ -0,0 +1,10 @@
+#include <curses.h>
+#include "../main.h"
+
+void safePrint(int y, int x, char *string, int color){
+    if(y < 0 || y >= height) return;
+    if(x < 0 || x >= width) return;
+
+    attron(COLOR_PAIR(color));
+    mvprintw(y, x, "%s", string);
+}
