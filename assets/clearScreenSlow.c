8b72d3e02a18fca7348cb915de976598c639a862
TerribleSuccess <146784390+TerribleSuccess@users.noreply.github.com>
2025-11-26 04:52:02 +0000

    switch to use global width, height

--- assets/clearScreenSlow.c
+++ assets/clearScreenSlow.c
@@ -3,15 +3,6 @@
 #include "../main.h"
 
 void clearScreenSlow(){
-    int width;
-    int height;
-    struct winsize wbuf;
-
-    if(ioctl(0, TIOCGWINSZ, &wbuf) != - 1){
-        width = wbuf.ws_col;
-        height = wbuf.ws_row;
-    }
-
     attron(COLOR_PAIR(COLOR_PAIR_BABYBLUE));
     for(int y = 0; y < height; y++){
         for(int x = 0; x < width; x++){
